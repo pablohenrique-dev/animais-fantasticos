92429a8e65c675d58d72b0475967a496ee8e7a0c
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-01-27 11:16:44 -0300

    correção bug script funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,35 +1,36 @@
 export default class Funcionamento {
-  constructor(funcionamento, activeClass){
-    this.funcionamento = document.querySelector(funcionamento);
+  constructor(functionamento, activeClass) {
+    this.funcionamento = document.querySelector(functionamento);
     this.activeClass = activeClass;
   }
 
-  dadosFuncionamento(){
+  dadosFuncionamento() {
     this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
     this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
   }
 
-  dadosAgora(){
+  dadosAgora() {
     this.dataAgora = new Date();
     this.diaAgora = this.dataAgora.getDay();
     this.horarioAgora = this.dataAgora.getUTCHours() - 3;
   }
 
-  estaAberto(){
+  estaAberto() {
     const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
-    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1]);
+    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
+      && this.horarioAgora < this.horarioSemana[1]);
     return semanaAberto && horarioAberto;
   }
-  
-  ativaAberto(){
-    if(this.estaAberto()){
+
+  ativaAberto() {
+    if (this.estaAberto()) {
       this.funcionamento.classList.add(this.activeClass);
     }
   }
-  
+
   init() {
-    if(this.funcionamento){
-      this.ativaAberto();
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
       this.dadosAgora();
       this.ativaAberto();
     }
